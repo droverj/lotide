fdb3eb7dcc6b7204d23f9d4987f60c9f2154d21a
Jodie Drover <jodiedrover@live.com>
2022-08-11 12:12:42 -0400

    Created a function called head for the lotide project which returns the first element of an array.

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
   if (actual !== expected) {
-    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
