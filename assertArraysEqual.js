06a70eb3190d9b9cd81994fee69f88e3bb01ec24
Jodie Drover <jodiedrover@live.com>
2022-08-13 13:21:11 -0400

    An assertion function which leverages the eqArrays function and  prints a message to the console stating if two arrays are the same or not.

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(actual, expected) {
+  for (const elmt in actual) {
+    if (actual[elmt] !== expected[elmt]) {
+      return false;
+      // loops through the first array and compares each element to the same index in the second array
+    }
+    if (expected.length !== actual.length) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (eqArrays(actual, expected) === false) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// assertArraysEqual([1,2,3], [1, 2, 3]);
+// assertArraysEqual([3,2,1], [1,2,3]);
+// assertArraysEqual([], undefined);
+// assertArraysEqual([""], []);
+// assertArraysEqual([1], ["1"]);
