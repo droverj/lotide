738a42a5199dc93557f4ca5d104ebe57d44a2c18
Jodie Drover <jodiedrover@live.com>
2022-08-15 16:14:59 -0400

    Implemented a function which takes in a string as an argument and returns on object which holds a count (values) of each time a letter (keys) in the string appears. Assertion test failed for a string with both capital letters and lower case letters.

---
+++ countLetters.js
@@ -0,0 +1,47 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function that takes in a sentence as a string
+// returns a count of each of the letters in that sentence
+// will return an object
+const countLetters = function(sentence) {
+  sentence.toLowerCase();
+
+  let result = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      // skip spaces
+      if (result[letter]) {
+        result[letter] += 1;
+        // if the letter already exists in result, add 1 to the value
+      } else {
+        result[letter] = 1;
+        // for each letter in the sentence, set a key of that letter to result with
+        // a value of 1 for each letter
+      }
+    }
+  }
+  // console.log(result);
+  return result;
+};
+
+
+const result1 = countLetters("hello");
+// { "h": 1, "e": 1, "l": 2, "o": 1 };
+
+assertEqual(result1["h"], 1); // h appears in hello once, compares the value in "h" key
+assertEqual(result1["l"], 2);
+
+
+const result2 = countLetters("lighthouse in the house");
+assertEqual(result2["e"], 3);
+assertEqual(result2[" "], undefined);
+
+const result3 = countLetters("wEiRd RiGhT");
+assertEqual(result3["r"], 2); // expected 2, returned undefined
