0b961282ba9ec33ce65ce1daa3b087a149b46ef1
Jodie Drover <jodiedrover@live.com>
2022-08-15 17:53:58 -0400

    Tinkered with the code and found how I'd be accessing the object's keys and values

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// function takes in an object and a value
+// scans the object => returns the first key which contains the given value
+// if no key with that value is found, it should return undefined
+const findKeyByValue = function(object, value) {
+  const result = ""
+  const keys = Object.keys(object);
+  const values = Object.values(object);
+
+  for (const key of keys) {
+    // console.log(object[key]) // The Wire
+    // console.log(key); // drama
+  }
+  return result;
+}
+
+
+// TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
