69d3099eda0b12a2e614a15ae608a2ad27bdc574
Jodie Drover <jodiedrover@live.com>
2022-08-15 15:32:32 -0400

    eslinted solution

--- countOnly.js
+++ countOnly.js
@@ -13,29 +13,22 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
 
-  // for (const item in itemsToCount) {
-  //   if (itemsToCount[item] === true) {
-  //     // console.log(item);
-  //   }
-  // }
   for (const item of allItems) {
     if (itemsToCount[item] === true) {
       // condition set to only increment results counter if the item in itemsToCount is true
-    if (results[item]) {
-      results[item] += 1;
-      // sets a property inside the results object with the string of 'item' as the key
-      // the value will be set to 1 for each item
-      // if the key appears again, it will increment by 1 each time
-    } else {
-      results[item] = 1;
-      // if the key does not appear again, the value will remain as 1
+      if (results[item]) {
+        results[item] += 1;
+        // sets a property inside the results object with the string of 'item' as the key
+        // the value will be set to 1 for each item
+        // if the key appears again, it will increment by 1 each time
+      } else {
+        results[item] = 1;
+        // if the key does not appear again, the value will remain as 1
+      }
     }
   }
-  }
-  // console.log(results);
-
   return results;
-}
+};
 // countOnly returns an object with a count on all the strings in allItems, the input array
 
 const firstNames = [
