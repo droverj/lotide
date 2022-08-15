b6f929bd02cf61c87df859f3aff94f34bdd96644
Jodie Drover <jodiedrover@live.com>
2022-08-15 17:25:12 -0400

    Updated solution to handle a few test cases such as an empty string or arguments that aren't strings. Eslinted function.

--- letterPositions.js
+++ letterPositions.js
@@ -23,20 +23,28 @@ const assertArraysEqual = function(actual, expected) {
 const letterPositions = function(sentence) {
   const results = {};
 
+  if (typeof sentence !== "string" || sentence === "") {
+    return undefined;
+  }
+
   sentence = sentence.toLowerCase();
 
   for (let i = 0; i < sentence.length; i++) {
-    if (results[sentence[i]]) {
-      results[sentence[i]].push(i);
-    } else {
-      results[sentence[i]] = [i];
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
     }
   }
-
-  console.log(results);
-
   return results;
 };
 
 // letterPositions("hello");
 assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("lighthouse in the house").h, [3, 5, 15, 18]);
+assertArraysEqual(letterPositions(""), undefined);
+assertArraysEqual(letterPositions(123), undefined);
+assertArraysEqual(letterPositions([]), undefined);
+assertArraysEqual(letterPositions("HeLlO").l, [2, 3]);
