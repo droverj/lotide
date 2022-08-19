a22edd879cbf51ee6bcda1e99e1719988412005d
Jodie Drover <jodiedrover@live.com>
2022-08-19 12:11:13 -0400

    So far only able to return a key up until an object within an object. I tried adding another object within an object and it returned undefined. Not sure if that was an error in my function call.

--- findKey.js
+++ findKey.js
@@ -32,3 +32,38 @@ results1 = findKey({
 }, x => x.stars === 2); // => "noma"
 
 console.log(results1);
+
+results2 = findKey({
+  string: "string",
+  numbers: 123,
+  numArr: [1, 2, 3],
+  words: ["Hello", "World"]
+}, x => Array.isArray(x));
+
+console.log(results2);
+
+results3 = findKey({
+  first: 
+    { second: "final"}
+}, x => x.second === "final"); // => first
+
+// function didn't work on third object within an object
+
+console.log(results3);
+
+results4 = findKey({
+  "Blue Hill":  { stars: 1 },
+  "Akaleri":    { stars: 3 },
+  "noma":       { stars: 2 },
+  "elBulli":    { stars: 3 },
+  "Ora":        { stars: 2 },
+  "Akelarre":   { stars: 3 }
+}, x => x.stars === 3); // => "Akaleri"
+
+assertEqual(results1, "noma");
+assertEqual(results2, "numArr");
+assertEqual(results3, "first");
+assertEqual(results4, "Akaleri");
+assertEqual(findKey({}, x => x = x), undefined);
+assertEqual(findKey(123, x => x = x), undefined);
+assertEqual(findKey([123], x => x * 2), undefined);
