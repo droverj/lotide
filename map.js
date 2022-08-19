247a443347383642836e221504cdbd546248ae70
Jodie Drover <jodiedrover@live.com>
2022-08-19 10:17:36 -0400

    Added test cases to map.js

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
+// A function which takes in an array and a callback.
+// It returns a modified version of the original array based on the callback.
 const map = function(array, callback) {
   const results = [];
   for (const item of array) {
@@ -10,8 +12,6 @@ const map = function(array, callback) {
 
 
 // TEST CODE
-const results1 = map(words, word => word[0]);
-console.log(results1);
 
 const eqArrays = function(actual, expected) {
   for (const elmt in actual) {
@@ -34,18 +34,22 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
+const results1 = map(words, word => word[0]);
 assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ])
 
 const emptyArray = [];
-
 const results2 = map(emptyArray, word => word);
-
 assertArraysEqual(results2, []);
 
-const oneWord = ["one"];
+const nums = [1, 2, 3, 4, 5];
+const results3 = map(nums, num => num * 2);
+assertArraysEqual(results3, [2, 4, 6, 8, 10]);
 
-const results3 = map(words, word => word === "control");
+const results4 = map(words, word => word.slice(1))
+assertArraysEqual(results4, [ 'round', 'ontrol', 'o', 'ajor', 'om' ]);
 
-assertArraysEqual(results3, ["e"]);
+const results5 = map(words, word => word[0].toUpperCase() + word.slice(1));
+assertArraysEqual(results5, ['Ground', 'Control', 'To', 'Major', 'Tom']);
 
-// work with a mentor on how to refactor this 
+const results6 = map(nums, num => num > 3);
+assertArraysEqual(results6, [false, false, false, true, true]);
