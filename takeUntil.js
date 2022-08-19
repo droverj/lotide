9565048a154603f2c11ba6f915e5c2033e95c252
Jodie Drover <jodiedrover@live.com>
2022-08-19 11:10:28 -0400

    Assertions passed. No eslint errors.

--- takeUntil.js
+++ takeUntil.js
@@ -4,7 +4,12 @@
 // The callback is only provided with one item from the array
 
 const takeUntil = function(array, callback) {
+  if (Array.isArray(array) === false) {
+    return undefined;
+  }
+
   const result = [];
+
   for (const item of array) {
     if (callback(item) === true) {
       return result;
@@ -43,8 +48,25 @@ const assertArraysEqual = function(actual, expected) {
 const numbers = [1, 2, 3, 4, 5, -5, -4, -3];
 
 const results1 = takeUntil(numbers, num => num < 0);
-console.log(results1);
+// console.log(results1);
 assertArraysEqual(results1, [1, 2, 3, 4, 5]);
 
 
 const words = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+const results2 = takeUntil(words, word => word === ",");
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
+
+const numbers2 = [0, -1, -2, -3, -4];
+
+const results3 = takeUntil(numbers2, num => num < 0);
+assertArraysEqual(results3, [0]);
+
+const results4 = takeUntil(numbers2, num => num === 0);
+assertArraysEqual(results4, []);
+
+const results5 = takeUntil(words, word => word.length > 4);
+assertArraysEqual(results5, ["I've", "been", "to"]);
+
+const results6 = takeUntil("hello", word => word[1]);
+assertArraysEqual(results6, undefined);
