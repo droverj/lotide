a0dd27d8f04ec6a155f3efddc5486d7bb7c477bb
Jodie Drover <jodiedrover@live.com>
2022-09-02 12:49:07 -0400

    Added a .gitignore file to ignore node_modules. Modified package.json so the scripts section would test using mocha. Modified the headTest.js file to use mocha and chai test assertions.

--- test/headTest.js
+++ test/headTest.js
@@ -1,13 +1,26 @@
 // test/headTest.js
+const assert = require('chai').assert;
+const head = require('../head');
 
-// const { head, assertEqual } = require('../head', '../assertEqual');
-const head = require('../head.js');
-const assertEqual = require('../assertEqual');
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["one"]), "one");
-assertEqual(head([]), undefined);
-assertEqual(head(["1"]), "1");
-assertEqual(head([1]), 1);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined when passed an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+  it("returns 'one' for ['one']", () => {
+    assert.strictEqual(head(['one']), 'one');
+  });
+
+});
