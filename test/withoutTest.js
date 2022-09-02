457a29270fc75e2b1dec5f05dce9ec7d21e5d80c
Jodie Drover <jodiedrover@live.com>
2022-09-02 19:09:41 -0400

    Refactored without function. I was only able to get it to remove one item from the source array. If two items matched in the itemToRemove array, the second one would be deleted.

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -4,8 +4,35 @@ const without = require('../without');
 
 // TEST CODE
 describe("#without", () => {
-  it("should return the first array but only include items that do not match with items in the second array", () => {
-    assert.deepEqual(without([1, 2, 3], [2, 3, 4]), 1);
+  it("should return the source array but with items not found in the itemToRemove array", () => {
+    assert.deepEqual(without([1,2,3], [1,4,5]), [2, 3]);
   });
 
+  it("should return the source array but with items not found in the itemToRemove array", () => {
+    assert.deepEqual(without(["Hello", "Lighthouse", "Labs"], ["Hello", "Hi", "Hey"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("should return the source array but with items not found in the itemToRemove array", () => {
+    assert.deepEqual(without([1, "3", 3], [2, 3, 5]), [1, "3"]);
+  });
+
+  it("should return the source array but with items not found in the itemToRemove array", () => {
+    assert.deepEqual(without([1, "3", 3], [2, "3", 5]), [1, 3]);
+  });
+
+  it("should return the source array but with items not found in the itemToRemove array", () => {
+    assert.deepEqual(without(["1", 3, 4, 7], ["1", "3", 5, 5, 6, "7"]), [3, 4, 7]);
+  });
+
+  it("should return an array with an empty string if the first array is an empty string", () => {
+    assert.deepEqual(without([""], ["1"]), [""]);
+  });
+
+  it("should return undefined if the source argument is not an array", () => {
+    assert.strictEqual(without(123, [4, 5, 6]), undefined);
+  });
+
+  it("should return a copy of the source array if no numbers match in the itemToRemove array", () => {
+    assert.deepEqual(without([1, 2, 3], [4, 5, 6]), [1, 2, 3]);
+  });
 });
--- without.js
+++ without.js
@@ -1,61 +1,30 @@
 // without.js
-const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual');
 
-// Function that takes in two arrays. An item that appears in both the first and second array
-// is removed from the source array.
-const without = function(source, itemToRemove) {
-  let filtered = [];
-  let removeStr;
-
-  for (const num of source) {
-    // for... of loop - returns a list of values of numeric properties
-    if (typeof source[num] === "number" && source[num] !== itemToRemove[num]) {
-      filtered.push(source[num]);
-      // source array is pushed into new array (filtered) if an element didnt also appear
-      //  in itemsToRemove array
-    }
-  }
-  if (typeof source[0] === "number") {
-    // if the first array is an array of numbers...
-    return filtered;
+// Function takes an array in each of the two args
+// An item that appears in both the first (source) and second array is removed from the source array
+// The modified source array is returned
+const without = (source, itemToRemove) => {
+  if (!Array.isArray(source)) {
+    return undefined;
   }
 
-  for (const elmt in itemToRemove) {
-    // for...in loop returns a list of keys on the object being iterated over
-    if (typeof itemToRemove[elmt] === "string" && typeof source[elmt] === "string") {
-      removeStr = itemToRemove.toString();
-      // console.log(removeStr);
+  let filterOut;
+  const newSource = [];
+  for (const item of itemToRemove) {
+    if (source.includes(item)) {
+      filterOut = item;
     }
   }
-  for (const elmt in source) {
-    if (source[elmt] !== removeStr) {
-      filtered.push(source[elmt]);
+  for (const item of source) {
+    if (item !== filterOut) {
+      newSource.push(item);
     }
   }
-  return filtered;
+  return newSource;
 };
 
-
-assertArraysEqual(without(["1", "2", "3"], ["2"]), ["1", "3"]);
-assertArraysEqual(without([10, 0, 1], [1]), [10, 0]);
-
-without(["1", "2", "3"], ["2"]); // => ["1", "3"]
-without(["hello", "world", "lighthouse"], ["world"]); // => ["hello", "lighthouse"]
-
-without([1, 2, 3], [1, "2", "3"]); // => [2, 3]
-without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
-without(["1"], ["1"]);
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]);
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
-
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
-assertArraysEqual(without(["hello", "world", "lighthouse"], ["lighthouse"]), ["hello", "world"]);
-
-assertArraysEqual(without([1, 2, 3], ["1", 2, 3]), [1]);
-assertArraysEqual(without([""], ["1"]), [""]);
-assertArraysEqual(without([], ["1"]), []);
-
+// console.log(without([1,3,3], [2,3,5])); // [1];
+// console.log(without([1,"3",3], [2,1,5])); // ["3", 3];
+// console.log(without(["1", "3", 3, 4], ["1", 5, 5, 6, "7"])); // ["3", 3, 4]
+// console.log(without([1, 2, 3], [4, 5, 6]));
 module.exports = without;
