ad90e756b9e14ad93eba26358677d047be1cb250
Jodie Drover <jodiedrover@live.com>
2022-09-02 14:12:48 -0400

    Modified tailTest.js to use mocha and chai for test code. Refactored tail function to pass all tests.

--- tail.js
+++ tail.js
@@ -1,4 +1,7 @@
 const tail = function(array) {
+  if (array.length < 2 || Array.isArray(array) === false) {
+    return undefined;
+  }
   return array.slice(1);
 };
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,25 +1,45 @@
 // test/tailTest.js
-
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
-// const { tail, assertEqual } = require('../tail', '../assertEqual')
 
 
 // Test Case 1: tail returns a new array and not a modified version of original array
-const words = ["Yo Yo", "Lighthouse", "Lab"];
-tail(words);
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Lab"];
+// tail(words);
+// assertEqual(words.length, 3);
 
-// Test Case 2: Confirms tail is returning desired output
-const test = tail(["this", "is", "a", "test"]);
-assertEqual(test[0], "is");
-assertEqual(test[1], "a");
-assertEqual(test.length, 3);
+// // Test Case 2: Confirms tail is returning desired output
+// const test = tail(["this", "is", "a", "test"]);
+// assertEqual(test[0], "is");
+// assertEqual(test[1], "a");
+// assertEqual(test.length, 3);
 
 
-// Test Case 3: Assertions for an empty array
-const empty = tail([]);
-assertEqual(empty[0], undefined);
-assertEqual(empty.length, 0);
+// // Test Case 3: Assertions for an empty array
+// const empty = tail([]);
+// assertEqual(empty[0], undefined);
+// assertEqual(empty.length, 0);
 
 // console.log(module)
+
+describe("#tail", () => {
+  it("should return ['Lighthouse', 'Lab'] for ['Yo Yo', 'Lighthouse', 'Lab']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Lab']), ['Lighthouse', 'Lab']);
+  });
+
+  it("should return [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("should return undefined for an empty array", () => {
+    assert.strictEqual(tail([]), undefined);
+  });
+
+  it("should return undefined for an array with less than two items", () => {
+    assert.strictEqual(tail([1]), undefined);
+  });
+
+  it("should return undefined if it is not an array", () => {
+    assert.strictEqual(tail("hello"), undefined);
+  });
+});
