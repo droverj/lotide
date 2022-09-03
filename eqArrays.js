11d95152f1e0bdbdeced5fc312bf1f26eca232b1
Jodie Drover <jodiedrover@live.com>
2022-09-03 18:56:47 -0400

    Added some comments and ran eslint

--- eqArrays.js
+++ eqArrays.js
@@ -10,18 +10,20 @@ const eqArrays = function(actual, expected) {
     return false;
   }
   for (const [index, value] of actual.entries()) {
+    // object destructuring with the entries() method
+    // entries method returns an object with key value pairs for each index
     if (Array.isArray(value)) {
+      // recursion begins
       if (!eqArrays(actual[index], expected[index])) {
+        // function will stop calling itself if it is not an array
+        // it will return false if one of the elements do not match
         return false;
       }
     } else if (actual[index] !== expected[index]) {
-      return false
+      return false;
     }
   }
   return true;
 };
-
-
-
 
 module.exports = eqArrays;
