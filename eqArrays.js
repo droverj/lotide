4cb1d835769b34a476744c8958fa499281cc105c
Jodie Drover <jodiedrover@live.com>
2022-09-03 18:22:34 -0400

    Was able to refactor my eqArrays function to handle comparisons of nested arrays using the recursive method thanks to tutoring from Sean Lee. Going to attempt to refactor it once more to use a for...of loop instead of a for...in loop.

--- eqArrays.js
+++ eqArrays.js
@@ -6,15 +6,30 @@ const eqArrays = function(actual, expected) {
   if (actual.length === 0) {
     return undefined;
   }
-  for (const elmt in actual) {
-    if (actual[elmt] !== expected[elmt]) {
-      return false;
-    }
-    if (expected.length !== actual.length) {
-      return false;
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (const item in actual) {
+    if (Array.isArray(actual[item])) {
+      if (!eqArrays(actual[item], expected[item])) {
+        return false;
+      }
+    } else if (actual[item] !== expected[item]) {
+      return false
     }
   }
   return true;
 };
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // true
+console.log(eqArrays([1, 3, 3], [1, 2, 3])); // false
+console.log(eqArrays([1, [3, 3]], [1, [3, 3]])); // true
+console.log(eqArrays([1, [2, 3]], [1, [3, 3]])); // false
+console.log(eqArrays([1, [3, 3], [4, 5, 6]], [1, [3, 3], [4, 5, 6]])); // true
+console.log(eqArrays([1, [3, 3, [4, 5, 6]]], [1, [3, 3, [4, 5, 6]]])); // true
+console.log(eqArrays([1, [3, 3, [4, 5, 6]]], [1, [3, 3, [4, 5, [6]]]])); // false
+
+
+
 
 module.exports = eqArrays;
